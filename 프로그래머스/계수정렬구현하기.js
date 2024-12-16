0857e22853ca4a9afef7227f18e0e08b5400331e
cindyCho <hjcho2303@naver.com>
2024-12-16 19:38:58 +0900

    계수 정렬 구현하기

--- 프로그래머스/계수정렬구현하기.js
+++ 프로그래머스/계수정렬구현하기.js
@@ -1,23 +1,16 @@
-function solution(s) {
-  // 각 문자열의 빈도수 저장하기
-  const counting = new Map();
-  let alphabet = [];
-  let word = s.split('');
-
-  word.map(a => {
-    if (counting.has(a)) {
-      counting.set(a, counting.get(a) + 1);
-    } else {
-      counting.set(a, 1);
-    }
-  });
-
-  alphabet = alphabet.sort((a, b) => a - b);
-  alphabet.map(a => a.charCodeAt());
-
-  word.map(a => alphabet.push(a.charCodeAt()));
-  console.log(alphabet);
-  // 각 문자를 아스키코드값으로 매핑하기
+function solution(word) {
+  // 1. 알파벳 숫자만큼 배열을 만든다.
+  let counts = Array(26).fill(0);
+  // 2. 해당하는 알파벳의 인덱스는 값을 더해준다.
+  for (count of word) {
+    counts[count.charCodeAt(0) - 'a'.charCodeAt(0)] += 1;
+  }
+  // 3. 알파벳 배열을 순회하면서 숫자가 0보다 크면 그 수만큼 숫자를 알파벳으로 변환하여 글자를 생성한다.
+  let answer = '';
+  for (let i = 0; i < 26; i++) {
+    answer += String.fromCharCode(i + 'a'.charCodeAt(0)).repeat(counts[i]);
+  }
+  return answer;
 }
 
 console.log(solution('hello'));
