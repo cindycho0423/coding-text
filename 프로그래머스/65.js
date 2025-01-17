10040ea2c297e07629bfaf42429d8d4e9a02aa9c
cindyCho <hjcho2303@naver.com>
2025-01-17 20:16:05 +0900

    65번 문제

--- 프로그래머스/65.js
+++ 프로그래머스/65.js
@@ -1,1 +1,22 @@
-//
+function solution(s) {
+  let zero = 0;
+  let transform = 0;
+
+  while (s !== '1') {
+    let filtered = s.split('').filter(a => {
+      if (a === '0') {
+        zero++;
+        return false;
+      }
+      return true;
+    });
+
+    s = filtered.length.toString(2);
+    transform++;
+  }
+
+  return [transform, zero];
+}
+
+console.log(solution('110010101001'));
+console.log(solution('01110'));
